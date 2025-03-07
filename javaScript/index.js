904420a6a45e21edbc212a1223855ae60163af56
“AbhilashReddy1519” <abhilashreddy6300@gmail.com>
2025-03-07 21:00:10 +0530

    Add alert for upcoming feature on sign-up buttons and update checkbox styles

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,7 +1,16 @@
 let signBtns = document.querySelectorAll('.signBtn');
 
-signBtns.forEach(signBtn => {
-    signBtn.addEventListener('click', () => {
-        window.location.href = './login.html';
+signBtns.forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent redirecting
+        alert("Feature Coming Soon!");
     });
 });
+
+let signBtn1 = document.querySelectorAll('.signBtn1');
+
+signBtn1.forEach(button => {
+    button.addEventListener('click', () => {
+        window.location.href = './login.html';
+    })
+})
