4b41d1925b42a80f0982c386d0c452c7a92b3dce
“AbhilashReddy1519” <abhilashreddy6300@gmail.com>
2025-03-07 22:36:27 +0530

    Add alert for upcoming features and improve styling for social icons and header

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,11 +1,25 @@
 let signBtns = document.querySelectorAll('.signBtn');
 
-signBtns.forEach(button => {
+document.querySelectorAll('.signBtn').forEach(button => {
     button.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent redirecting
-        alert("Feature Coming Soon!");
+        event.preventDefault();
+        
+        let alertBox = document.getElementById("topAlert");
+        alertBox.classList.remove("hidden");
+        alertBox.classList.add('flex');
+
+        // Hide after 600 milli seconds
+        setTimeout(() => {
+            alertBox.classList.add("hidden");
+        }, 600);
     });
 });
+
+// Function to close manually
+function closeAlert() {
+    document.getElementById("topAlert").classList.add("hidden");
+}
+
 
 let signBtn1 = document.querySelectorAll('.signBtn1');
 
